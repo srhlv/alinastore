680f3024833812a97b3de2facc712f5a60e34d6b
Serhii Levandovskyi <srhlv@MacBook-Pro-Serhii.local>
2026-07-05 18:18:09 +0300

    chore: remove _check.sh script

--- _check.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-echo "=== CHECK START ==="
-pwd
-which git 2>&1 && git --version 2>&1
-which gh 2>&1 && gh --version 2>&1
-cd /Users/srhlv/Projects/alina-store && git status 2>&1
-echo "=== CHECK END ==="
